905cae75c9f2a79084591293f3c6d9ab163aa3c0
IrinaMuravyova <miomir@yandex.ru>
2023-07-17 14:16:36 +0800

    добавлена функция, меняющая контакт

--- src/actions/change_contacts.py
+++ src/actions/change_contacts.py
@@ -9,7 +9,6 @@ def delete_contact() -> None:
 
     searching_contact = input('Введите какой контакт удалить(имя или фамилию или отчество или номер телефона): ')
     temp_lst = [line for line in catalog_copy if searching_contact  not in line]
-    print(temp_lst)
 
     with open(FILE_PATH, 'w') as catalog:
         for line in temp_lst:
@@ -17,16 +16,31 @@ def delete_contact() -> None:
 
 
 def change_contact() -> None:
-    # with open("data.txt", 'r') as catalog:
-    #     catalog_copy = catalog.readlines()
-    pass
+    
+    with open(FILE_PATH, 'r') as catalog:
+        catalog_copy = catalog.readlines()
+
+    temp_lst=[]
+
+    changing_contact = input('Введите какой контакт хотите изменить(ФИО): ')
+    new_fio = input('Введите новое значение ФИО: ')
+    new_phone_number = input('Введите новое значение номера телефона: ')
+    
+    for line in catalog_copy:
+        if changing_contact.lower() in line.lower():
+            line = f'{new_fio}, {new_phone_number}'
+        temp_lst.append(line)    
+
+    with open(FILE_PATH, 'w') as catalog:
+        for line in temp_lst:
+            catalog.write(line)
 
 def add_contact() -> None:
 
-    name = input("Введите имя: ")
     surname = input("Введите фамилию: ")
+    name = input("Введите имя: ")
     middlename = input("Введите отчество: ")
     phone_number= input("Введите номер телефона с кодом страны начиная с +: ")
 
     with open(FILE_PATH, 'a') as catalog:
-        catalog.write(f'\n{name} {surname} {middlename}, {phone_number}')
+        catalog.write(f'\n{surname} {name} {middlename}, {phone_number}')
