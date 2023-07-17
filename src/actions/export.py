137d1b5586c00a349e05d76fc236605d91ce3b30
IrinaMuravyova <miomir@yandex.ru>
2023-07-17 15:00:03 +0800

    добавлен экспорт в csv

--- src/actions/export.py
+++ src/actions/export.py
@@ -1,4 +1,20 @@
-from const import FILE_PATH
+from const import FILE_PATH, FILE_PATH_CSV
+import csv
 
 def save_contact() -> None:
-    pass
+    choose = int(input('\nС каким расширением хотите сохранить файл:'
+                   '\n1: .txt'
+                   '\n2: .csv'
+                   '\n'))
+    match choose:
+        case 1:
+            pass
+        case 2:
+            with open(FILE_PATH, 'r') as catalog:
+                catalog_copy = catalog.readlines()
+
+            with open(FILE_PATH_CSV, 'w', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerows(catalog_copy)
+            
+            print("Файл экспортирован.")
